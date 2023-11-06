10431d8075bcab7152dab82a27c091b7a1ef9932
nmoyski <78941586+nmoyski@users.noreply.github.com>
2023-11-06 01:44:49 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -60,7 +60,7 @@ document.querySelector("#slider").addEventListener("input", function() {
 });
 
 document.querySelector("#vintage").addEventListener("click", function() {
-	video.classList.toggle("oldSchool");
+	video.classList.add("oldSchool");
 });
 
 document.querySelector("#orig").addEventListener("click", function() {
